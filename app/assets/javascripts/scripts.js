d8e0562a6883c793a1b804ac7467680126da60ab
robertSahm <robertsahm@gmail.com>
2013-02-26 00:22:40 -0500

    bustin it out

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,77 @@
+
+// $(function() {
+// 	$('.blog_post').find('.tagged a').css('color', "green");
+// 	$('.blog_post').addClass('addedClassByatch');
+// });
+
+
+// if(jQuery('.tagged').attr('class') =="") {
+//        alert($('#id1').attr('class'));
+//   }
+
+
+// $(function() {
+//   $('.tagged(jamaica)')
+// });
+
+
+// $(function() {
+// 	$('.tagged').find('a').do(function() {
+// 		alert('working?');
+// 	});
+// });
+
+// $(function() {
+// 	$('a').click(function(){
+//   	if ($('.tagged').text().indexOf('jamaica') !== -1){
+//     ('.blog_post').addClass("funkyAssClass");
+//   	}
+// 	});
+// });
+
+// $(function() {
+// 	$('a[href^="/blog/tagged/7/jamaica"]').click(function() {
+// 	alert('holla, bitches');
+
+// 	});
+// });
+
+// $(function() {
+// 	$('.blog_post').find('a[href^="/blog/tagged/7/jamaica"]').each(function() {
+// 		$('.blog_post').addClass('tagJamaica');
+// 	});
+// });
+
+// var jamaicaTag = ('a[href^="/blog/tagged/7/jamaica"]');
+
+// $(function() {
+// 	if (
+// 		$('.blog_post').find(jamaicaTag).text(function() {
+// 			$('.blog_post').addClass('tagJamaica');
+// 		})
+// 	);
+// });
+
+// $(function() {
+// 	if(
+// 		$("#blog_posts > .blog_post:contains('music')").length > 0) {
+// 	    $('.blog_post').addClass('tagJamaica');
+// 		}
+// });
+
+$(function() {
+	//if ($(".header h1 a:contains('music')").length);
+			$(".blog_post:contains('mix')").addClass('tagMix');
+});
+
+$(function() {
+	//if ($(".header h1 a:contains('jamaica')").length);
+			$(".blog_post:contains('photo')").addClass('tagPhoto');
+});
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -20,12 +20,15 @@ Porta::Application.routes.draw do
   # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
   root :to => 'refinery/blog/posts#index'
   # match '/home', to: 'home#home'
-
   mount Refinery::Core::Engine, :at => '/'
+  
   # match 'radio', to: 'home#radio'
   # match ':action' => 'static#:action'
   # The priority is based upon order of creation:
   # first created -> highest priority.
+
+
+
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
