56e32255b84fe10236b2b3eef10123cd6c375e60
robertSahm <robertsahm@gmail.com>
2013-03-14 15:19:32 -0400

    heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,12 @@ Porta::Application.routes.draw do
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
-  # root :to => 'refinery/calendar/events#index'
+  # 
   root :to => 'refinery/blog/posts#index'
-
-  # match '/home', to: 'home#home'
+  # match :to => 'refinery/calendar/events#index
+  # match '/', to: 'refinery/calendar/events#index'
   mount Refinery::Core::Engine, :at => '/'
-  
+
   # match 'radio', to: 'home#radio'
   # match ':action' => 'static#:action'
   # The priority is based upon order of creation:
