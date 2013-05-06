d193dcb213eabbafe8df68c6d5831908968d1002
robertSahm <robertsahm@gmail.com>
2013-05-06 17:40:45 -0400

    changed s3 region

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "sprockets/railtie"
 ENV['S3_KEY']='AKIAJXRGVLJTXEXHMEWQ'
 ENV['S3_SECRET']='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'
 ENV['S3_BUCKET']='portaphotos'
-S3_REGION='Oregon'
+S3_REGION='us-east-1'
 
 
 
