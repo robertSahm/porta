de27a7d17a73ecc4da2afcee24dd2570021b21d9
robertSahm <robertsahm@gmail.com>
2013-07-11 15:25:09 -0400

    new fixes...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,9 @@ Porta::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  
 
 
 end
+
+Refinery::Core.config.s3_backend = true
